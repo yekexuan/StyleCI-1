1cd0d7ad6798eb916e2ae5c5d0d5d6673134077a
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-02-18 02:47:26 -0600

    Homepage design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('title', 'The PHP Coding Style Continuous Integration Service')
 
-@section('content')
-<header id="top" class="header">
+@section('top')
+<header id="top" class="home-header">
     <div class="text-vertical-center">
         <h1>StyleCI</h1>
         <h3>The PHP Coding Style Continuous Integration Service</h3>
@@ -15,28 +15,74 @@
         @endif
     </div>
 </header>
+<div class="highlights">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                    <div class="panel-image">
+                        <i class="fa fa-5x fa-code-fork"></i>
+                    </div>
+                    <div class="panel-body">
+                        <h3 class="featurette-heading">Analyse</h3>
+                        <p>We watch all your pull requests.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                    <div class="panel-image">
+                        <i class="fa fa-5x fa-code"></i>
+                    </div>
+                    <div class="panel-body">
+                        <h3>Patch</h3>
+                        <p>Keep your code aligned with coding standards.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                    <div class="panel-image">
+                        <i class="fa fa-5x fa-gear"></i>
+                    </div>
+                    <div class="panel-body">
+                        <h3>Configure</h3>
+                        <p>Fix PSR-1, PSR-2, PSR-5, symfony and more.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
 
-@section('css')
-<style>
-.text-vertical-center {
-    display: table-cell;
-    text-align: center;
-    vertical-align: middle;
-}
-
-.text-vertical-center h1 {
-    margin: 0;
-    padding: 0;
-    font-size: 4.5em;
-    font-weight: 700;
-}
-
-.header {
-    padding-top: 100px;
-    display: table;
-    position: relative;
-    width: 100%;
-}
-</style>
+@section('content')
+<div class="features">
+    <div class="row featurette">
+        <div class="col-sm-6">
+            <img class="featurette-image img-responsive" src="{{ url('img/analyse-repo.jpg') }}" alt="Analyse" />
+        </div>
+        <div class="col-sm-6">
+            <h2 class="featurette-heading">Analyse pull requests</h2>
+            <p class="lead">Analyse all your pull requests and see the results alsong side any other continuous integration services such as travis right on GitHub.</p>
+        </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+        <div class="col-sm-6">
+            <h2 class="featurette-heading">Apply style fixes patch</h2>
+            <p class="lead">Download patch files for the coding style fixes ready to use, and apply them automatically using "git apply" or can be inspected via the browser too.</p>
+        </div>
+        <div class="col-sm-6">
+            <img class="featurette-image img-responsive" src="{{ url('img/patch-commit.jpg') }}" alt="Patch">
+        </div>
+    </div>
+    <hr>
+    <div class="row featurette">
+        <div class="col-sm-12">
+            <h2 class="featurette-heading">Configure to your needs</h2>
+            <p class="lead">Powered by php-cs-fixer, and is fully configurable to fix PSR-1, PSR-2, PSR-5, symfony, and various other checks via a .php_cs file that can be commited to your repo.</p>
+        </div>
+    </div>
+</div>
 @stop
