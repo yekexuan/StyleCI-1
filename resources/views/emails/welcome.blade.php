395529ca4ecfa00a731a947144965f131d1d254f
James Brooks <jbrooksuk@me.com>
2015-02-18 15:14:05 +0000

    Friendlier email
    Closes #156.
    
    Signed-off-by: Graham Campbell <graham@mineuk.com>

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <p>Dear {{ $name }},</p>
-<p>Thank you for creating an account on <a href="{{ route('home') }}">StyleCI</a>.</p>
-<p>StyleCI is powered by PHP CS Fixer, and is totally configurable via an optional .php_cs file, committed to your repo. It might be very useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer.</p>
-<p>If you have any questions at all, feel free to contact me by emailing graham@mineuk.com, or by creating an <a href="https://github.com/StyleCI/StyleCI/issues/new">issue</a> on GitHub.</p>
+<p>Thank you for creating an account on <a href="{{ route('home') }}">StyleCI</a>, we're excited to have you on board!</p>
+<p>StyleCI is powered by <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">PHP CS Fixer</a>; this means that it's totally configurable via an optional <code>.php_cs</code> file which has is committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">https://github.com/FriendsOfPHP/PHP-CS-Fixer</a>.</p>
+<p>If you have any questions at all, feel free to contact me by emailing <a href="mailto:graham@mineuk.com?subject=StyleCI Support">graham@mineuk.com</a>, or by creating an <a href="https://github.com/StyleCI/StyleCI/issues/new">issue</a> on GitHub.</p>
 @stop
