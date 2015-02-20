f33887fb0b3b9ae498a9a695edeb9a54cd13c5ce
Graham Campbell <graham@mineuk.com>
2015-02-20 18:39:37 +0000

    Fixed text based emails

--- resources/views/emails/failed-text.blade.php
+++
@@ -1,6 +0,0 @@
-@extends(Config::get('core.text'))
-
-@section('content')
-The coding style analysis of your commit "{{ $commit }}" on repo "{{ $repo }}" revealed problems.
-You can see the details at {{ $link }}.
-@stop
---
+++ resources/views/emails/failed-text.php
@@ -0,0 +1,7 @@
+Dear <?php echo e($name); ?>,
+
+The coding style analysis of your commit "<?php echo e($commit); ?>" on repo "<?php echo e($repo); ?>" revealed problems.
+You can see the details at <?php echo e($link); ?>.
+
+Thank you,
+StyleCI
