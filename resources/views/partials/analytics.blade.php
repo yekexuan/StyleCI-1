8bae9191be54a5dd378c78f3ef63bfda15dc1158
Graham Campbell <graham@mineuk.com>
2015-02-20 20:57:56 +0000

    Closes #123

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,9 +1,21 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '{!! Config::get('analytics.googleid') !!}');
-  ga('send', 'pageview');
+@if(Config::get('analytics.googleanalytics'))
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{{ Config::get('analytics.googleanalytics') }}');
+ga('send', 'pageview');
 </script>
+@endif
+@if(Config::get('analytics.userreport'))
+<script type="text/javascript">
+var _urq = _urq || [];
+_urq.push(['initSite', {{ Config::get('analytics.userreport') }}]);
+(function() {
+var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+})();
+</script>
+@endif
