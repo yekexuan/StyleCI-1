f8e874371321f399080855a01a5cb184f77b114e
Graham Campbell <graham@mineuk.com>
2015-02-20 13:04:52 +0000

    Updated welcome email

--- resources/views/emails/welcome-html.blade.php
+++ resources/views/emails/welcome-html.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <p>Thank you for creating an account on <a href="{{ route('home') }}">StyleCI</a>, we're excited to have you on board!</p>
-<p>StyleCI is powered by <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">PHP CS Fixer</a>; this means that it's totally configurable via an optional <code>.php_cs</code> file which has is committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">https://github.com/FriendsOfPHP/PHP-CS-Fixer</a>.</p>
+<p>StyleCI is powered by <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">PHP CS Fixer</a>, and is totally configurable via an optional <code>.styleci.yml</code> file which can be committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">https://github.com/FriendsOfPHP/PHP-CS-Fixer</a>.</p>
 <p>If you have any questions at all, feel free to contact me by emailing <a href="mailto:graham@mineuk.com?subject=StyleCI Support">graham@mineuk.com</a>, or by creating an <a href="https://github.com/StyleCI/StyleCI/issues/new">issue</a> on GitHub.</p>
 @stop
--- resources/views/emails/welcome-text.blade.php
+++ resources/views/emails/welcome-text.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 Thank you for creating an account on StyleCI, we're excited to have you on board!
-StyleCI is powered by PHP CS Fixer; this means that it's totally configurable via an optional .php_cs file which has is committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer.
+StyleCI is powered by PHP CS Fixer, and is totally configurable via an optional .styleci.yml file which can be committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer.
 If you have any questions at all, feel free to contact me by emailing graham@mineuk.com, or by creating a new issue on GitHub at https://github.com/StyleCI/StyleCI/issues/new.
 @stop
