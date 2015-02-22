331d1d986ededae51c154b2f4bdbc34d84f9dc1c
Graham Campbell <graham@mineuk.com>
2015-02-22 19:48:00 +0000

    Derp fix

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -24,5 +24,4 @@
     <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
     <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>
-@endif
 @stop
