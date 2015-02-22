dcda33f259ac3337db1194a7995c6d2e5986cc5d
James Brooks <jbrooksuk@me.com>
2015-02-22 12:18:05 +0000

    Added GoSquared Analytics

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -8,6 +8,16 @@ ga('create', '{{ Config::get('analytics.googleanalytics') }}');
 ga('send', 'pageview');
 </script>
 @endif
+@if(Config::get('analytics.gosquared'))
+<script>
+!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+insertBefore(d,q)}(window,document,'script','_gs');
+
+_gs('{{ Config::get('analytics.gosquared') }}');
+</script>
+@endif
 @if(Config::get('analytics.userreport'))
 <script type="text/javascript">
 var _urq = _urq || [];
