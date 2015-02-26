9a5494bd957afb65c25e004b40c902d903487a2f
Graham Campbell <graham@mineuk.com>
2015-02-26 22:53:47 +0000

    Improved our welcome email

--- resources/views/emails/welcome-html.blade.php
+++ resources/views/emails/welcome-html.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <p>Thank you for creating an account on <a href="{{ route('home') }}">StyleCI</a>; we're excited to have you on board!</p>
-<p>StyleCI is powered by <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">PHP CS Fixer</a>, and is totally configurable via an optional <code>.styleci.yml</code> file which can be committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">https://github.com/FriendsOfPHP/PHP-CS-Fixer</a>.</p>
-<p>If you have any questions at all, feel free to contact me by emailing <a href="mailto:graham@mineuk.com?subject=StyleCI Support">graham@mineuk.com</a>, or by creating an <a href="https://github.com/StyleCI/StyleCI/issues/new">issue</a> on GitHub.</p>
+<p>StyleCI is powered by <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">PHP CS Fixer</a>, and is totally configurable via an optional <code>.styleci.yml</code> file which can be committed to your repo. You can read all about this over on our blog: <a href="https://blog.styleci.io/redefining-configuration">https://blog.styleci.io/redefining-configuration</a>.</p>
+<p>It might be useful for you to check out PHP CS Fixer's documentation before continuing at <a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer">https://github.com/FriendsOfPHP/PHP-CS-Fixer</a> regarding what each fixer actually does.</p>
+<p>If you have any questions at all, feel free to reach out to us by emailing <a href="mailto:support@styleci.io?subject=StyleCI Support">support@styleci.io</a>, or by creating an <a href="https://github.com/StyleCI/StyleCI/issues/new">issue</a> on GitHub.</p>
 @stop
--- resources/views/emails/welcome-text.php
+++ resources/views/emails/welcome-text.php
@@ -1,8 +1,9 @@
 Dear <?php echo e($name); ?>,
 
 Thank you for creating an account on StyleCI; we're excited to have you on board!
-StyleCI is powered by PHP CS Fixer, and is totally configurable via an optional .styleci.yml file which can be committed to your repo. It might be useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer.
-If you have any questions at all, feel free to contact me by emailing graham@mineuk.com, or by creating a new issue on GitHub at https://github.com/StyleCI/StyleCI/issues/new.
+StyleCI is powered by PHP CS Fixer, and is totally configurable via an optional .styleci.yml file which can be committed to your repo. You can read all about this over on our blog: https://blog.styleci.io/redefining-configuration.
+It might be useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer regarding what each fixer actually does.
+If you have any questions at all, feel free to reach out to us by emailing support@styleci.io, or by creating a new issue on GitHub at https://github.com/StyleCI/StyleCI/issues/new.
 
 Thank you,
 StyleCI
