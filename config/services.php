4343c972a450f59dbba5035c5d46924707face50
Graham Campbell <graham@mineuk.com>
2015-03-16 23:00:38 +0000

    Fixed logins

--- config/services.php
+++ config/services.php
@@ -44,10 +44,4 @@ return [
         'secret' => '',
     ],
 
-    'github' => [
-        'client_id'     => env('GITHUB_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect'      => env('APP_URL').'/auth/github-callback',
-    ],
-
 ];
