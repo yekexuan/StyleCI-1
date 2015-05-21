0a823ddcf68a5a76de8da87e7c073bdb35078f1f
Graham Campbell <graham@mineuk.com>
2015-05-21 17:53:27 +0100

    Updated header

--- config/exceptions.php
+++ config/exceptions.php
@@ -1,9 +1,10 @@
 <?php
 
 /*
- * This file is part of Laravel Exceptions.
+ * This file is part of StyleCI.
  *
  * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
