a89b926020859afe25ecea4e8a6f4ff2a552c83d
Graham Campbell <graham@alt-three.com>
2015-06-26 10:34:04 +0100

    Tweaked the disabled email

--- resources/views/emails/disabled-html.blade.php
+++ resources/views/emails/disabled-html.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <p>The repo "{{ $repo }}" has been disabled and removed from our platform.</p>
-<p>If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable your repositories.</p>
+<p>If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repository.</p>
 @stop
--- resources/views/emails/disabled-text.php
+++ resources/views/emails/disabled-text.php
@@ -1,7 +1,7 @@
 Dear <?php echo e($name); ?>,
 
 The repo "<?php echo e($repo); ?>" has been disabled and removed from our platform.
-If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable your repositories.
+If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repository.
 
 Thank you,
 StyleCI
