b661fe69c6b912430ac4b02431993df8e171ab5d
Graham Campbell <graham@alt-three.com>
2015-06-27 17:02:47 +0100

    Tweaked the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,7 +7,6 @@
                     <li><a href="{{ route('privacy_policy') }}">Privacy</a></li>
                     <li><a href="{{ route('security_policy') }}">Security</a></li>
                     <li><a href="{{ route('about_us') }}">About</a></li>
-                    <li><a href="mailto:support@alt-three.com">Contact</a></li>
                 </ul>
             </div>
             <div class="col-sm-2 logo">
@@ -16,9 +15,9 @@
             <div class="col-sm-5 social-links">
                 <ul class="footer-links">
                     <li><a href="https://status.styleci.io" target="_blank">Status</a></li>
-                    <li><a href="https://blog.styleci.io" target="_blank">Blog</a></li>
                     <li><a href="https://twitter.com/teamstyleci" target="_blank">Twitter</a></li>
                     <li><a href="https://github.com/StyleCI" target="_blank">GitHub</a></li>
+                    <li><a href="mailto:support@alt-three.com">Contact</a></li>
                 </ul>
             </div>
         </div>
