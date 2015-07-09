865f9d1b4a28121a6eaf27df586529fb7c189279
Graham Campbell <graham@alt-three.com>
2015-07-09 12:06:20 +0100

    Added missing imports

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -14,6 +14,8 @@ namespace StyleCI\StyleCI\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Str;
+use StyleCI\StyleCI\Http\Middleware\BodySize;
+use StyleCI\StyleCI\Http\Middleware\EventHeader;
 use StyleCI\StyleCI\Models\Repo;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
