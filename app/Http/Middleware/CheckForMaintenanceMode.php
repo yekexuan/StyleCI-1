0c97e64c280bd79f2e594061edb6ff86c9908a07
Graham Campbell <graham@alt-three.com>
2015-07-10 17:31:42 +0100

    Throw an exception for maintenance mode

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -13,9 +13,8 @@ namespace StyleCI\StyleCI\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory as View;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * This is the check for maintenance mode middleware class.
@@ -32,24 +31,15 @@ class CheckForMaintenanceMode
     protected $app;
 
     /**
-     * The view factory instance.
-     *
-     * @var \Illuminate\Contracts\View\Factory
-     */
-    protected $view;
-
-    /**
      * Create a new check for maintenance mode instance.
      *
      * @param \Illuminate\Contracts\Foundation\Application $app
-     * @param \Illuminate\Contracts\View\Factory           $view
      *
      * @return void
      */
-    public function __construct(Application $app, View $view)
+    public function __construct(Application $app)
     {
         $this->app = $app;
-        $this->view = $view;
     }
 
     /**
@@ -63,7 +53,7 @@ class CheckForMaintenanceMode
     public function handle(Request $request, Closure $next)
     {
         if ($this->app->isDownForMaintenance()) {
-            return new Response($this->view->make('maintenance')->render(), 503);
+            throw new HttpException(503);
         }
 
         return $next($request);
