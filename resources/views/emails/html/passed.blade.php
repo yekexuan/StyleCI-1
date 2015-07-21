4d5632a6b5606d1c67ba2fb5c4e6423dd321776a
James Brooks <james@bluebaytravel.co.uk>
2015-07-21 12:42:18 +0100

    Update email content

--- resources/views/emails/html/passed.blade.php
+++ resources/views/emails/html/passed.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.email')
 
 @section('content')
-<p>The coding style analysis of "{{ $commit }}" on the "{{ $branch }}" branch of "{{ $repo }}" has passed after previous analyses on this branch were failing.</p>
+<p>The coding style analysis of <b>{{ $commit }}</b> on the <b>{{ $branch }}</b> branch on <b>{{ $repo }}</b> has passed after previous analyses on this branch were failing.</p>
 <p>Click <a href="{{ $link }}">here</a> to see the details.</p>
 @stop
