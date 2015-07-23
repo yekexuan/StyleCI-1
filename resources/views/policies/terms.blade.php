e749492231ba83b25789439d28c1b9f2dd34ee75
Graham Campbell <graham@alt-three.com>
2015-07-23 22:35:06 +0100

    Updated terms

--- resources/views/policies/terms.blade.php
+++ resources/views/policies/terms.blade.php
@@ -5,14 +5,97 @@
 @section('content')
 <h1>Terms of Service</h1>
 
-<hr>
+<p>Alt Three Services Limited ("StyleCI", "Alt Three", "we", "us" or "our") agrees to furnish services to the Subscriber (the "Subscriber", "you" or "your"), subject to the following Terms of Service. Use of StyleCI's service constitutes acceptance and agreement to StyleCI's Terms of Service.</p>
 
-<h2>General Information</h2>
+<p>StyleCI reserves the right to modify the Terms of Service without notice.</p>
 
-<p class="lead">Our terms of service will be available shortly.</p>
-<p>Are you looking for our privacy policy?. View it <a href="{{ route('privacy_policy') }}">here</a>.</p>
+<h3>Use of Services</h3>
 
-<h2>Questions</h2>
+<p>You may use our services, provided that you are of legal age to form a binding contract and are not barred from receiving such services under the laws of the United Kingdom. In order to access our services, you are required to provide current and factual identification, contact, and other information as part of the registration process. You are responsible for the confidentiality of your account information and for all activities that occur under your account. You are solely responsible for all content within your account. You agree to immediately notify StyleCI of any unauthorized use of your account or any other breach of security. StyleCI will not be liable for any loss or damage as a result of your failure to provide us with accurate information or to keep your account secure.</p>
 
-<p>Any questions about the Terms of Service should be addressed to <a href="mailto:support@alt-three.com">support@alt-three.com</a>.</p>
+<h3>Acceptable Conduct</h3>
+
+<p>You are responsible for the actions of all users of your account and any data that is created, stored, displayed by, or transmitted by your account while using StyleCI. You will not engage in any activity that interferes with or disrupts StyleCI's services or networks connected to StyleCI.</p>
+
+<h3>Prohibited Usage</h3>
+
+<p>You agree that any of the below activities are considered prohibited usage and will result in immediate account suspension or cancellation without a refund and the possibility that StyleCI will impose fees; and/or pursue civil remedies without providing advance notice.</p>
+
+<p>Access to Other Computers or Networks without Authorization: Attempting unauthorized and/or illegal access of computers, networks and/or accounts not belonging to party seeking access. Any act which interferes with the services of another user or network. Any act relating to the circumvention of security measures.</p>
+
+<p>StyleCI and the services it provides may only be used for lawful purposes. Transmission, distribution, or storage of any information, data or material in violation of United States or state regulation or law, or by the common law, is prohibited. This includes, but is not limited to, material protected by copyright, trademark, trade secret, or other intellectual property rights. StyleCI's services may not be used to facilitate infringement of these laws in any way.</p>
+
+<p>Other Activities viewed as Illegal or Harmful: Engaging in illegal activities or engaging in activities harmful to the operations of StyleCI or StyleCI's customers.</p>
+
+<p>Providing False Data on any Contract or Application: including fraudulent use of credit card numbers.</p>
+
+<h3>Invoicing And Payment</h3>
+
+<p>You agree that StyleCI shall be permitted to charge your credit card on a monthly, annual, or other agreed upon basis in advance of providing services. Payment is due upon invoicing. Service may be interrupted on accounts that reach 3 days past due. Accounts that are not collectable by StyleCI may be turned over to an outside collection agency for collection.</p>
+
+<p>Subscriber is aware that StyleCI may prospectively change the specified rates and charges from time to time.</p>
+
+<p>StyleCI is not responsible for any additional bank fees, interest charges, finance charges, over draft charges, or other fees resulting from charges billed by StyleCI. Currency exchange settlements will be based on agreements between you and the provider of your credit card. Pro-rated credits will not be issued for unused services when customer retains any active service.</p>
+
+<h3>Server Provider Fees</h3>
+
+<p>You agree that StyleCI is not responsible for any additional fees charged to your credit card by third-party server providers such as Laravel Forge/Envoyer, DigitalOcean, Linode, Amazon, or Rackspace.</p>
+
+<h3>Support Boundries</h3>
+
+<p>StyleCI, provides technical support to our subscribers via our e-mail support system. The following are our guidelines when providing support: StyleCI provides support related to your repos being analysed for given standards. StyleCI does not offer technical support for application specific issues such as application configuration, programming, web or mail server configuration, or any other such issue.</p>
+
+<h3>Account Cancellation Or Suspension</h3>
+
+<p>StyleCI will use reasonable care in notifying the Customer and in resolving the problem in a method resulting in the least amount of service interference. StyleCI reserves the right to terminate service without notice. If inappropriate activity is detected, all accounts of the Customer in question will be deactivated until an investigation is complete. Prior notification to the Customer is not assured. In extreme cases, law enforcement will be contacted regarding the activity.</p>
+
+<p>If at any time it becomes necessary for StyleCI to cancel a customer's service without cause, StyleCI will provide 7 days advance notice.</p>
+
+<p>You may cancel the service at any time by using the "Delete Account" link located on the "Profile" sub-tab of the StyleCI web application. Cancellation of service does not relieve Subscriber of responsibility for the payment of all accrued charges.</p>
+
+<h3>Disclosure To Law Enforcement</h3>
+
+<p>The Terms of Service specifically prohibits the use of our service for illegal activities. Therefore, Subscriber agrees that StyleCI may disclose any and all subscriber information including assigned IP numbers, account history, account use, etc. to any court who sends us a valid Court Order, without further consent or notification to the Subscriber. In addition, StyleCI shall have the right to terminate all service set forth in this Agreement.</p>
+
+<h3>Server Security</h3>
+
+<p>Subscriber is solely responsible for maintaining the security of Subscriber's servers that are created via the StyleCI service.</p>
+
+<p>Any security vulnerabilities that you may find must be immediately reported to <a href="mailto: support@alt-three.com?subject=Security">support@alt-three.com</a></p>
+
+<h3>Warranty Disclaimer</h3>
+
+<p>We have no special relationship with or fiduciary duty to you. You acknowledge that we have no duty to take any action regarding: which Subscribers gain access to the Services; what Content you access via the Services; or how you may interpret or use the Content.</p>
+
+<p>You release us from all liability for you having acquired or not acquired Content through the Services. We make no representations concerning any Content contained in or accessed through the Services, and we will not be responsible or liable for the accuracy, copyright compliance, or legality of material or Content contained in or accessed through the Services.</p>
+
+<p>THE SERVICES AND CONTENT ARE PROVIDED "AS IS", "AS AVAILABLE" AND WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES IMPLIED BY ANY COURSE OF PERFORMANCE OR USAGE OF TRADE, ALL OF WHICH ARE EXPRESSLY DISCLAIMED. WE, AND OUR DIRECTORS, EMPLOYEES, AGENTS, SUPPLIERS, PARTNERS AND CONTENT PROVIDERS DO NOT WARRANT THAT: (I) THE SERVICES WILL BE SECURE OR AVAILABLE AT ANY PARTICULAR TIME OR LOCATION; (II) ANY DEFECTS OR ERRORS WILL BE CORRECTED; (III) ANY CONTENT OR SOFTWARE AVAILABLE AT OR THROUGH THE SERVICES IS FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS; OR (IV) THE RESULTS OF USING THE SERVICES WILL MEET YOUR REQUIREMENTS. YOUR USE OF THE SERVICES IS SOLELY AT YOUR OWN RISK.</p>
+
+<h3>Limitation Of Liability</h3>
+
+<p>IN NO EVENT SHALL WE, NOR OUR DIRECTORS, EMPLOYEES, AGENTS, PARTNERS, SUPPLIERS OR CONTENT PROVIDERS, BE LIABLE UNDER CONTRACT, TORT, STRICT LIABILITY, NEGLIGENCE OR ANY OTHER LEGAL OR EQUITABLE THEORY WITH RESPECT TO THE SERVICES (I) FOR ANY LOST PROFITS, DATA LOSS, DATA THEFT, COST OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR SPECIAL, INDIRECT, INCIDENTAL, PUNITIVE, COMPENSATORY OR CONSEQUENTIAL DAMAGES OF ANY KIND WHATSOEVER, SUBSTITUTE GOODS OR SERVICES (HOWEVER ARISING), (II) FOR ANY BUGS, VIRUSES, TROJAN HORSES, OR THE LIKE (REGARDLESS OF THE SOURCE OF ORIGINATION), OR (III) FOR ANY DIRECT DAMAGES IN EXCESS OF (IN THE AGGREGATE) OF FEES PAID TO US FOR THE PARTICULAR SERVICES DURING THE IMMEDIATELY PREVIOUS ONE MONTH PERIOD, EVEN IF STYLECI HAD BEEN ADVISED OF, KNEW, OR SHOULD HAVE KNOWN, OF THE POSSIBILITY THEREOF. SUBSCRIBER ACKNOWLEDGES THAT THE FEES PAID BY HIM OR HER REFLECT THE ALLOCATION OF RISK SET FORTH IN THIS AGREEMENT AND THAT STYLECI WOULD NOT ENTER INTO THIS AGREEMENT WITHOUT THESE LIMITATIONS. SUBSCRIBER HEREBY WAIVES ANY AND ALL CLAIMS AGAINST STYLECI ARISING OUT OF SUBSCRIBER'S PURCHASE OR USE OF THE SERVICES, OR ANY CONDUCT OF STYLECI’S DIRECTORS, OFFICERS, EMPLOYEES, AGENTS OR REPRESENTATIVES. YOUR SOLE AND EXCLUSIVE RIGHT AND REMEDY IN CASE OF DISSATISFACTION WITH THE SERVICES OR ANY OTHER GRIEVANCE SHALL BE YOUR TERMINATION AND DISCONTINUATION OF ACCESS TO OR USE OF THE SERVICES.</p>
+
+<h3>Idemnity</h3>
+
+<p>StyleCI wishes to emphasize that in agreeing to the StyleCI Terms of Service, customer indemnifies StyleCI for any violation of the Terms of Service that results in loss to StyleCI or the bringing of any claim against StyleCI by any third-party. This means that if StyleCI is sued because of a customer's or a customer of a customer's activity, the customer will pay any damages awarded against StyleCI, plus all costs and reasonable attorney's fees.</p>
+
+<h3>Notice</h3>
+
+<p>You agree that StyleCI may provide you with notices, including those regarding changes to the Terms of Service, by email, regular mail, or postings on StyleCI services.</p>
+
+<h3>Entire Agreement</h3>
+
+<p>The Terms of Service (including any policies, guidelines or amendments that may be presented to you from time to time) constitute the entire agreement between you and StyleCI and govern your use of StyleCI services, superceding any prior agreements between you and StyleCI for the use of StyleCI services.</p>
+
+<h3>Choice Of Law And Forum</h3>
+
+<p>The Terms of Service and the relationship between you and StyleCI shall be governed by the laws of The United Kingdom without regard to its conflict of law provisions. You and StyleCI agree to submit to the personal and exclusive jurisdiction of the courts located within The United Kingdom.</p>
+
+<h3>Waiver And Severability Of Terms</h3>
+
+<p>The failure of StyleCI to exercise or enforce any right or provision of the Terms of Service shall not constitute a waiver of such right or provision. If any provision of the Terms of Service is found by a court of competent jurisdiction to be invalid, the parties nevertheless agree that the court should endeavor to give effect to the party's intentions as reflected in the provision, and the other provisions of the Terms of Service remain in full force and effect.</p>
+
+<h3>Statute Of Limitations</h3>
+
+<p>You agree that regardless of any statute or law to the contrary, any claim or cause of action arising out of or related to use of StyleCI services or the Terms of Service must be filed within one (1) year after such claim or cause of action arose or be forever barred.</p>
 @stop
