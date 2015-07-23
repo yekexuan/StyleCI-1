d24b5643a5b73f259c7c0e970ae653fcd7728784
Graham Campbell <graham@alt-three.com>
2015-07-24 00:32:50 +0100

    Fixed inconsistent wording in emails

--- resources/views/emails/html/disabled.blade.php
+++ resources/views/emails/html/disabled.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <p>The repo <b>{{ $repo }}</b> has been disabled and removed from our platform.</p>
-<p>If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repository.</p>
+<p>If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repo.</p>
 @stop
--- resources/views/emails/text/disabled.blade.php
+++ resources/views/emails/text/disabled.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 The repo "{{ $repo }}" has been disabled and removed from our platform.
-If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repository.
+If this was all a mistake, don't fear! You can log back into StyleCI via GitHub and re-enable the repo.
 @stop
