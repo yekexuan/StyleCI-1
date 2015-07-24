6c823bb854a42babc64264b08f89fdb9c3e5f230
Graham Campbell <graham@alt-three.com>
2015-07-24 01:32:36 +0100

    Updated welcome email

--- resources/views/emails/text/welcome.blade.php
+++ resources/views/emails/text/welcome.blade.php
@@ -3,6 +3,5 @@
 @section('content')
 Thank you for creating an account on StyleCI; we're excited to have you on board!
 StyleCI is powered by PHP CS Fixer, and can be configured via an optional .styleci.yml file committed to the root of your repo.
-It might be useful for you to check out PHP CS Fixer's documentation before continuing at https://github.com/FriendsOfPHP/PHP-CS-Fixer regarding what each fixer actually does.
-If you have any questions at all, feel free to contact us by emailing support@alt-three.com.
+Our full documentation is available at https://styleci.readme.io/, and if you have any questions at all, feel free to contact us by emailing support@alt-three.com.
 @stop
