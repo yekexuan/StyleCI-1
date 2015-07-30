0cae359c8a4de49a6a727adc2d09aaa6f315741e
Graham Campbell <graham@alt-three.com>
2015-07-30 20:47:15 +0100

    Added git config

---
+++ config/git.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of StyleCI.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Remote Git User
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the remote git user to use.
+    |
+    */
+
+    'remote' => 'git@github.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Git Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the git name to use.
+    |
+    */
+
+    'name' => 'StyleCI Bot',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Git Email
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the git email to use.
+    |
+    */
+
+    'email' => 'bot@styleci.io',
+
+];
