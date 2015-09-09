7889623b3ad0042591d22430ada6510192bd41a0
Graham Campbell <graham@alt-three.com>
2015-09-09 19:52:25 +0100

    Move cookie warning to top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -31,7 +31,7 @@
 <![endif]-->
 
 <script type="text/javascript">
-    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"{{ route('privacy_policy') }}","theme":"light-bottom"};
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"{{ route('privacy_policy') }}","theme":"light-top"};
 </script>
 
 <script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
