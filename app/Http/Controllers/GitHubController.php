930441616cd9baba6b69205728f9228abfcca5c4
Graham Campbell <graham@alt-three.com>
2015-10-31 16:13:35 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -14,7 +14,6 @@ namespace StyleCI\StyleCI\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Str;
 use StyleCI\StyleCI\Http\Middleware\BodySize;
 use StyleCI\StyleCI\Http\Middleware\EventHeader;
 use StyleCI\StyleCI\Models\Repo;
