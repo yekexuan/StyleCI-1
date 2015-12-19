b06b50f2359e1e15c455391f98304e4e973e64e3
Graham Campbell <graham@alt-three.com>
2015-12-19 14:22:46 +0000

    Upgraded fixer

---
+++ config/fixer.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of StyleCI.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | The Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the analysis timeout. Set this to false to not have
+    | a timeout. Also note that no timeout will be set unless the needed pcntl
+    | functions are available.
+    |
+    */
+
+    'timeout' => 1800,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profiling
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify if blackfire profiling is enabled. Note that if the
+    | blackfire extension is not installed, enabling profiling will do nothing.
+    |
+    */
+
+    'profiling' => false,
+
+];
